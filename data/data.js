5a0df216590861e8ff4c4c750b8903763c3910e7
3247361336 <31716525+3247361336@users.noreply.github.com>
2018-07-08 16:51:32 +0800

    first commit

---
+++ data/data.js
@@ -0,0 +1,44 @@
+var postList = [{
+	object: {
+		date: "Jan 28 2017"
+	},
+	title: "小时候的冰棍儿与雪糕",
+	postImg: "/images/post/post-4.jpg",
+	avatar: "/images/avatar/avatar-5.png",
+	content: "冰棍和雪糕绝对不是同一种东西。3到5毛的雪糕犹如现在的哈根达斯,而5分到1毛的冰棍儿就像现在的老冰棍。时过境迁。。。。",
+	readingNum: 92,
+	collectionNum: {
+		array: [108]
+	},
+	commentNum: 7
+}, {
+	object: {
+		date: "Jan 28 2017"
+	},
+	title: "记忆里的春节",
+	postImg: "/images/post/post-1.jpg",
+	avatar: "/images/avatar/avatar-1.png",
+	content: "年少时，有几样东西。。。。",
+	readingNum: 93,
+	collectionNum: {
+		array: [155]
+	},
+	commentNum: 9
+}, {
+	object: {
+		date: "Jan 28 2017"
+	},
+	title: "从童年呼啸而过的火车",
+	postImg: "/images/post/post-3.jpg",
+	avatar: "/images/avatar/avatar-4.png",
+	content: "小时候，家的后面有。。。。",
+	readingNum: 99,
+	collectionNum: {
+		array: [100]
+	},
+	commentNum: 8
+}]
+// 必须向外部暴露模块接口
+module.exports = {
+	postList:postList
+}
